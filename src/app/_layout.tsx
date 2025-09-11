685ba41282eb51ed3bf8f0c3dab734dde940427c
LucasC-Pinheiro <pinheirotj69@gmail.com>
2025-09-11 18:06:02 -0300

    fix(layout): ajustar header e inserir logo

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,10 +11,10 @@ export default function Layout() {
     <>
       <StatusBar translucent backgroundColor="transparent" />
       {/* Test-only: small logo + app name in top-left */}
-      <View className="absolute top-10 left-0 right-0 z-50 mt-20 flex-row justify-center items-center">
+      {/* <View className="absolute top-10 left-0 right-0 z-50 mt-20 flex-row justify-center items-center">
         <LogoTest size={29} />
           <Text className="text-white ml-2 font-bold text-2xl">FlowPay</Text>
-      </View>
+      </View> */}
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)/sign-in" />
         <Stack.Screen name="(auth)/sign-up" />
